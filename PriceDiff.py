1bef66b518a551dc901cf3f0a378391c93d938ec
ChimeraBlack1 <christopherparke@zoho.com>
2020-01-02 10:15:11 -0500

    fixed alignment of cell writing

--- PriceDiff.py
+++ PriceDiff.py
@@ -46,19 +46,19 @@ for x in range(0, xlListEnd):
       newCost5 = newSheet.cell_value(y,18) # S
       newOutCost = newSheet.cell_value(y,19) # T
       newMSRP = newSheet.cell_value(y,20) # U
-      newSpecial1 = newSheet.cell_value(y,31)
-      newSpecial2 = newSheet.cell_value(y,32)
+      newSpecial1 = newSheet.cell_value(y,31) #AF
+      newSpecial2 = newSheet.cell_value(y,32) #AG
 
       #old
-      itemType = sheet.cell_value(x,0)
-      partNumber = sheet.cell_value(x,4)
-      model = sheet.cell_value(x,15)
-      cost5 = sheet.cell_value(x,18)
-      outCost = sheet.cell_value(x,19)
-      MSRP = sheet.cell_value(x,20)
-      special1 = sheet.cell_value(x,31)
-      special2 = sheet.cell_value(x,32)
-      desc = sheet.cell_value(x,30)
+      itemType = sheet.cell_value(x,0) #A
+      partNumber = sheet.cell_value(x,4) #E
+      model = sheet.cell_value(x,15) #P
+      cost5 = sheet.cell_value(x,18) #S
+      outCost = sheet.cell_value(x,19) #T
+      MSRP = sheet.cell_value(x,20) #U
+      special1 = sheet.cell_value(x,31) #AF
+      special2 = sheet.cell_value(x,32) #AG
+      desc = sheet.cell_value(x,29) #AD
       
       if cost5 != newCost5:
         print("old cost5: " + str(cost5) + " new newCost5:" + str(newCost5))
@@ -79,31 +79,31 @@ for x in range(0, xlListEnd):
       if newPrice == True:
         print("the price for " + str(serialNumberToTest) + " has changed")
         print("..............................................")
-        wst.write(modelStart, 0, itemType)
-        wst.write(modelStart, 3, thisMapp)
-        wst.write(modelStart, 11, serialNumberToTest)
+        wst.write(modelStart, 0, itemType) #A
+        wst.write(modelStart, 3, thisMapp) #D
+        wst.write(modelStart, 11, serialNumberToTest) # L
         if itemType == "Model":
-          wst.write(modelStart, 4, partNumber)
-          wst.write(modelStart, 5, "Equipment")
-          wst.write(modelStart, 6, category)
-          wst.write(modelStart, 28, "Document Direction Ltd")
+          wst.write(modelStart, 4, partNumber) #E
+          wst.write(modelStart, 5, "Equipment") #F
+          wst.write(modelStart, 6, category) #G
+          wst.write(modelStart, 28, "Document Direction Ltd") #AC
         if itemType == "Access":
           wst.write(modelStart, 8, "ACCESSORY") # "I"
           wst.write(modelStart, 9, "N") #"J"
         wst.write(modelStart, 13, model) #"N"
-        wst.write(modelStart, 14, 0)
-        wst.write(modelStart, 15, 0)
-        wst.write(modelStart, 16, 0)
-        wst.write(modelStart, 17, 0)
-        wst.write(modelStart, 19, newCost5) #"S"
-        wst.write(modelStart, 20, outCost) #"T"
-        wst.write(modelStart, 21, MSRP) # "U"
-        wst.write(modelStart, 25, 0)
-        wst.write(modelStart, 29, desc)
-        wst.write(modelStart, 30, special1)
-        wst.write(modelStart, 32, special2)
+        wst.write(modelStart, 14, 0) #O
+        wst.write(modelStart, 15, 0) #P
+        wst.write(modelStart, 16, 0) #Q
+        wst.write(modelStart, 17, 0) #R
+        wst.write(modelStart, 18, newCost5) #"S"
+        wst.write(modelStart, 19, outCost) #"T"
+        wst.write(modelStart, 20, MSRP) # "U"
+        wst.write(modelStart, 25, 0) #Z
+        wst.write(modelStart, 29, desc) #AD
+        wst.write(modelStart, 31, special1) #AE
+        wst.write(modelStart, 32, special2) #AG
         for l in range(0,38):
-            wst.write(modelStart,33+l, 0)
+            wst.write(modelStart,33+l, 0) 
 
         modelStart = modelStart + 1
         newPrice = False
